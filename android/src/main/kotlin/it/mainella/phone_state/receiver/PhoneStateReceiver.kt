3e18a42300a8b7be27d49fe16137d6f1458fdc0c
yan <yan.p@fideware.by>
2023-03-20 16:13:31 +0300

    add cancel call event

--- android/src/main/kotlin/it/mainella/phone_state/receiver/PhoneStateReceiver.kt
+++ android/src/main/kotlin/it/mainella/phone_state/receiver/PhoneStateReceiver.kt
@@ -1,9 +1,19 @@
 package it.mainella.phone_state.receiver
 
+import android.app.NotificationManager
+import android.app.StatusBarManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.service.notification.StatusBarNotification
+import android.telecom.CallRedirectionService
+import android.telecom.InCallService
+import android.telecom.PhoneAccountHandle
+import android.telephony.CellSignalStrengthWcdma
 import android.telephony.TelephonyManager
+import androidx.annotation.RequiresApi
 import it.mainella.phone_state.utils.PhoneStateStatus
 
 open class PhoneStateReceiver : BroadcastReceiver() {
@@ -15,6 +25,16 @@ open class PhoneStateReceiver : BroadcastReceiver() {
                 TelephonyManager.EXTRA_STATE_OFFHOOK -> PhoneStateStatus.CALL_STARTED
                 TelephonyManager.EXTRA_STATE_IDLE -> PhoneStateStatus.CALL_ENDED
                 else -> PhoneStateStatus.NOTHING
+            }
+            @RequiresApi(Build.VERSION_CODES.Q)
+            object : CallRedirectionService() {
+                override fun onPlaceCall(
+                    handle: Uri,
+                    initialPhoneAccount: PhoneAccountHandle,
+                    allowInteractiveResponse: Boolean
+                ) {
+                    status = PhoneStateStatus.CALL_ENDED
+                }
             }
         } catch (e: Exception) {
             e.printStackTrace()
