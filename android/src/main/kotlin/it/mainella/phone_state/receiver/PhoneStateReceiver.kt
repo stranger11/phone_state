fc895299e7bcb8d57068ddf92ec0d65eb0689a56
yan <yan.p@fideware.by>
2023-03-21 18:12:14 +0300

    revert changes

--- android/src/main/kotlin/it/mainella/phone_state/receiver/PhoneStateReceiver.kt
+++ android/src/main/kotlin/it/mainella/phone_state/receiver/PhoneStateReceiver.kt
@@ -2,6 +2,7 @@ package it.mainella.phone_state.receiver
 
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.TELEPHONY_SERVICE
 import android.content.Intent
 import android.telephony.TelephonyManager
 import it.mainella.phone_state.utils.PhoneStateStatus
@@ -14,6 +15,9 @@ open class PhoneStateReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         try {
+            val telephonyManager = context?.getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
+
             status = when (intent?.getStringExtra(TelephonyManager.EXTRA_STATE)) {
 
                 TelephonyManager.EXTRA_STATE_RINGING -> PhoneStateStatus.CALL_INCOMING
